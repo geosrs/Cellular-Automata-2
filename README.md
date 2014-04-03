c6ebb856dc496efd47bf4bcef1da6deb58dd2c26
Rushy Panchal <panchal.rushy@gmail.com>
2014-04-03 09:36:40 -0400

    Added tasks and new interface

--- README.md
+++ README.md
@@ -3,6 +3,17 @@ Cellular-Automata
 
 Nonlinear Dynamics Cellular Automata Project 2014
 
+Current Tasks:
+ - Krish: Interface design
+ - Rushy: Interface creation
+ - George: Create 2D CA test program
+ - Naomi: Convert George's 1D CA program to use classes
+ - Allan: Work on description of CA for "About CA" screen
+
+I still think we should use HTML + CSS + JavaScript for the interface, and then embed
+a Java applet with the actual CA. George/Naomi would have to learn Java graphics,
+but the overall app would look MUCH better.
+ 
 Notes on Commits:
 
  - Create a new commit for any change (so we have a running record of changes)
--- src/main.py
+++ src/main.py
@@ -25,7 +25,8 @@ class WindowManager(object):
 		
 	def open(self, window, replace = True, **options):
 		'''Opens a window'''
-
+		# this will close the older window
+		# if we want to just switch screens, then use a Frame per "screen", pack to show it, unpack the screen when it's done (to show another)
 		if 'inherit' not in options:
 			options['inherit'] = False
 		appClass = self.windows.get(window, NOT_FOUND)
@@ -50,23 +51,34 @@ class StartScreen(tk.BaseCustomWindow):
 		
 	def createInterface(self):
 		'''Creates the main interface'''
-		self.padx, self.pady = 25, 25
 		self.mainFrame = tk.Frame(self.master)
-		self.subFrame = tk.Frame(self.mainFrame)
-		self.aboutButton = tk.Button(self.mainFrame, text = "About CA", command = lambda: self.WM.open(ABOUT))
-		self.creditsButton = tk.Button(self.mainFrame, text = "Credits", command = lambda: self.WM.open(CREDITS))
-		self.startButton = tk.Button(self.mainFrame, text = "Start", command = lambda: self.WM.open(MAIN_PROGRAM))
-		self.helpButton = tk.Button(self.mainFrame, text = "Help", command = lambda: self.WM.open(HELP))
-		self.historyButton = tk.Button(self.subFrame, text = "User History", command = lambda: self.WM.open(HISTORY))
-		self.exitButton = tk.Button(self.subFrame, text = "Exit", command = self.close, style = "Quit.TButton")
-		self.subFrame.gridWidgets([(self.exitButton, self.historyButton)], padx = self.padx, pady = self.pady)
-		self.mainFrame.gridWidgets([
-			(None, self.aboutButton),
-			(self.creditsButton, self.startButton, self.helpButton),
-			self.subFrame,
-			], padx = self.pady, pady = self.pady)
+		self.leftFrame = tk.Frame(self.mainFrame)
+		self.rightFrame = tk.Frame(self.mainFrame)
+		self.centerFrame = tk.Frame(self.mainFrame)
+		self.mainLabel = tk.Label(self.mainFrame, text = NAME)
+		self.aboutButton = tk.Button(self.leftFrame, text = "About CA", command = lambda: self.WM.open(ABOUT))
+		self.creditsButton = tk.Button(self.leftFrame, text = "Credits", command = lambda: self.WM.open(CREDITS))
+		self.startButton = tk.Button(self.rightFrame, text = "Start", command = lambda: self.WM.open(MAIN_PROGRAM))
+		self.helpButton = tk.Button(self.rightFrame, text = "Help", command = lambda: self.WM.open(HELP))
+		self.historyButton = tk.Button(self.mainFrame, text = "User History", command = lambda: self.WM.open(HISTORY))
+		self.exitButton = tk.Button(self.mainFrame, text = "Exit", command = self.close, style = "Quit.TButton")
 		
-		self.mainFrame.grid(padx = 15, pady = 15)
+		self.aboutButton.pack(side = "top")
+		self.creditsButton.pack(side = "bottom")
+		self.historyButton.pack(side = "top")
+		self.exitButton.pack(side = "bottom")
+		self.startButton.pack(side = "top")
+		self.helpButton.pack(side = "bottom")
+		self.leftFrame.pack(side = "left", fill = "both")
+		self.rightFrame.pack(side = "right", fill = "both")
+		# self.centerFrame.pack(side = "top", fill = "both")
+		# self.mainFrame.gridWidgets([
+			# (self.aboutButton, self.historyButton, self.startButton),
+			# (None, self.mainLabel),
+			# (self.creditsButton, self.exitButton, self.helpButton)
+			# ])
+		
+		self.mainFrame.pack(side = "top", fill = "both", expand = True, ipadx = 15)
 	
 class CAWindow(tk.BaseCustomWindow):
 	'''Houses the entire Cellular Automata interface'''
