6527f61dc5be9c6d9c38312a3d8f1bee0e1840cd
Rushy Panchal <panchal.rushy@gmail.com>
2014-04-04 11:19:08 -0400

    Automatic updating to HTML canvas

--- test/graphics.js
+++ test/graphics.js
@@ -3,28 +3,92 @@
 // Copyright 2014
 // Models Python's graphics.py
 
-function GraphWin(id, options) {
+var FILL = "fill";
+var STROKE = "stroke";
+
+function GraphWin(id, userOptions) {
 	// Creates a GraphWin Object
-	canvas = document.getElementById(id);
-	canvas.width = is_null(options.width) ? canvas.width: options.width;
-	canvas.height = is_null(options.height) ? canvas.height: options.height;
-	return {
+	options = {
+		background: "black",
+		};
+	for (var option in userOptions) {
+		options[option] = userOptions[option];
+		}
+	var canvas = document.getElementById(id);
+	var graph = {
 		canvas: canvas,
-		draw: this.canvas.getContext("2d"),
-		setColor: function(color) {
-			this.draw.fillStyle = color;
-			}
+		draw: canvas.getContext("2d"),
+		width: canvas.width,
+		height: canvas.height,
+		colors: {
+			"black": "#000000",
+			"white": "#FFFFFF",
+			},
+			
+		setColor: function(color, method) {
+			// Sets the current drawing color
+			method = is_null(method) ? FILL: method;
+			var hexColorAttempt = this.colors[color];
+			var hexColor = is_null(hexColorAttempt) ? color: hexColorAttempt;
+			if (method == FILL) {
+				this.draw.fillStyle = is_null(hexColor) ? this.draw.fillStyle: hexColor;
+				}
+			else {
+				this.draw.strokeStyle = is_null(hexColor) ? this.draw.strokeStyle: hexColor;
+				}
+			return hexColor;
+			},
+		
 		setBackground: function(color) {
-			// Changes the background color
+			// Changes the background color --- this will delete everything on the canvas
 			this.setColor(color);
 			this.draw.fillRect(0, 0, this.canvas.width, this.canvas.height);
 			},
+			
+		clear: function () {
+			// Clears the canvas
+			this.draw.clearRect(0, 0, this.canvas.width, this.canvas.height);
+			this.setBackground("white");
+			},
+			
 		plot: function(x, y, color) {
 			// Colors the pixel (x, y) a specific color
 			this.setColor(color);
 			this.draw.fillRect(x, y, 1, 1);
-			}
+			},
+			
+		getHeight: function() {
+			// Returns the height of the GraphWin
+			return this.canvas.height;
+			},
+			
+		getWidth: function() {
+			// Returns the width of the GraphWin
+			return this.canvas.width;
+			},
+			
+		setHeight: function(height) {
+			// Sets the GraphWin's height
+			height = is_null(height) ? canvas.height: height;
+			this.canvas.height = height;
+			this.height = height;
+			return height;
+			},
+			
+		setWidth: function(width) {
+			// Sets the GraphWin's width
+			width = is_null(width) ? canvas.width: width;
+			this.canvas.width = width;
+			this.width = width;
+			return width;
+			},
 		};
+	
+	graph.setBackground(options.background);
+	graph.setWidth(options.width);
+	graph.setHeight(options.height);
+	
+	return graph;
 	}
 
 function is_null(x) {
