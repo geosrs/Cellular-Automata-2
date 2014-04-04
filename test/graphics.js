659269d8dbd56648362865889ca5d7ba8446394f
Rushy Panchal <panchal.rushy@gmail.com>
2014-04-03 20:47:59 -0400

    Added prototype graphics.js
    This is a JavaScript port of the standard graphics.py in Python

---
+++ test/graphics.js
@@ -0,0 +1,33 @@
+// graphics.js
+// Rushy Panchal
+// Copyright 2014
+// Models Python's graphics.py
+
+function GraphWin(id, options) {
+	// Creates a GraphWin Object
+	canvas = document.getElementByid(id);
+	canvas.width = is_null(options.width) ? canvas.width: options.width;
+	canvas.height = is_null(options.height) ? canvas.height: options.height;
+	return {
+		canvas: canvas,
+		draw: this.canvas.getContext("2d"),
+		setColor: function(color) {
+			this.draw.fillStyle = color;
+			}
+		setBackground: function(color) {
+			// Changes the background color
+			this.setColor(color);
+			this.draw.fillRect(0, 0, this.canvas.width, this.canvas.height);
+			},
+		plot: function(x, y, color) {
+			// Colors the pixel (x, y) a specific color
+			this.setColor(color);
+			this.draw.fillRect(x, y, 1, 1);
+			}
+		};
+	}
+
+function is_null(x) {
+	// Checks whether or not the variable is null
+	return x == null || x == "undefined";
+	}
